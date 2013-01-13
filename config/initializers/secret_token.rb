6288eac09d13f0bb0725c2b146353b093c252f1f
Cara Borenstein <cjborenstein@gmail.com>
2013-01-12 22:57:24 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cytosite::Application.config.secret_token = '90924816b611ffbb18553c3db03f1aad722f31a19b1c92a639e9783c2d0a9177c816cfad95cc90be6c336ac97e5165b22296f6d70931b84b467b89a094a226ba'
