1be7f030bab223e02ccf176ee74512461e404c7a
cjb2182@columbia.edu <cjb2182@columbia.edu>
2013-01-13 19:46:03 -0500

    remove spec cytosite

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,6 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Cytosite'" do
-      visit '/static_pages/home'
-      page.should have_content('Cytosite')
     end
   end
 end
