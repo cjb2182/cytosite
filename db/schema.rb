a9e67b51966750c77f24c03e7e2a2701c6d629bb
cjb2182@columbia.edu <cjb2182@columbia.edu>
2013-01-27 15:51:09 -0500

    Authentication Back

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130114001804) do
+ActiveRecord::Schema.define(:version => 20130127190045) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -23,11 +23,24 @@ ActiveRecord::Schema.define(:version => 20130114001804) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+
+  create_table "userscyto", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
