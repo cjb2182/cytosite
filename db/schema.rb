43e2d7947f3b57567e96f055382ca980c13f86d8
cjb2182@columbia.edu <cjb2182@columbia.edu>
2013-01-28 21:37:31 -0500

    Finish user edit, update, index, and destroy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130127190045) do
+ActiveRecord::Schema.define(:version => 20130129022904) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20130127190045) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
