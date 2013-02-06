32f9edf317ca53d3871277f5d8639fa74af27c40
cjb2182@columbia.edu <cjb2182@columbia.edu>
2013-02-05 21:22:03 -0500

    static pages not visible without signin

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  before_filter :signed_in_user, only: [:home, :images]
+  before_filter :signed_in_user, only: [:home, :images, :overview, :new]
 
   def home
   end
