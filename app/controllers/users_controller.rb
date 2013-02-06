10615b7b344e8b0647c54dd568a23cc6f1636ef5
cjb2182@columbia.edu <cjb2182@columbia.edu>
2013-02-05 21:52:37 -0500

    Users require signin, signin has home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user
 
   def show
     @user = User.find(params[:id])
@@ -34,25 +34,9 @@ class UsersController < ApplicationController
   end
 
   private
-
+    
     def signed_in_user
-      flash[:notice] = "Please sign in."
-      redirect_to signin_url
-   end
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
 
-  #  def signed_in_user
-  #    unless signed_in?
-  #      store_location
-  #      redirect_to signin_url, notice: "Please sign in."
-  #    end
-  #  end
-
-    #def correct_user
-    #  @user = User.find(params[:id])
-    #  redirect_to(root_path) unless current_user?(@user)
-    #end
-  
-    #def admin_user
-    #  redirect_to(root_path) unless current_user.admin?
-    #end
 end
