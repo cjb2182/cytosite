4acfa622f35a5254f7afe3e4620fea101540b7bd
cjb2182@columbia.edu <cjb2182@columbia.edu>
2013-02-06 20:39:02 -0500

    added gravatar to image

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,7 +1,7 @@
 module ImagesHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given image.
-  def gravatar_for(image)
+  def gravatar_for_image(image)
     gravatar_id = Digest::MD5::hexdigest(image.description.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: image.description, class: "gravatar")
